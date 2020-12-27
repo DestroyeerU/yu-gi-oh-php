76ea7a10ac50b4f1bdc3719e49c4901d20316603
DestroyeerU <destroyeeru18@gmail.com>
2020-12-27 09:32:11 -0300

    Paginas de carta e deck criadas

---
+++ carta.php
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+  <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="styles/global.css">
+  <link rel="stylesheet" href="styles/partida.css">
+
+  <title>Cadastro de Cartas</title>
+</head>
+<body>
+  <h1>Registro de Cartas</h1><br>
+
+  <form action="salvarCarta.php" method="POST" enctype="multipart/form-data">
+
+    Foto:<br>
+    <input name="userfile" type="file" /><br>
+
+    Nome:<br>
+    <input type="text" name="nome"><br>
+
+    Descriçao:<br>
+    <textarea id="w3review" name="w3review" rows="8" cols="30"></textarea><br>
+
+    Tipo:<br>
+    <select name="time">
+      <option value="monster">Monstro</option>
+      <option value="spell">Magia</option>
+      <option value="trap">Armadilha</option>
+    </select>
+
+
+  </form>
+
+</body>
+</html>
---
+++ deck.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+  <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="styles/global.css">
+  <link rel="stylesheet" href="styles/partida.css">
+
+  <title>Cadastro de Decks</title>
+</head>
+<body>
+
+  <h1>Registro de Decks</h1><br>
+
+  <form action="salvarDeck.php" method="POST" enctype="multipart/form-data">
+
+    Nome:<br>
+    <input type="text" name="d1"><br>
+
+    Cartas:<br>
+    <img src="download.jpg">
+    <img src="download.jpg">
+    <img src="download.jpg">
+
+  </form>
+
+</body>
+</html>
